e55cf9384ac38c3c71e9cdb23b7684e450a39c6b
iamrifki <iamrifki0@gmail.com>
2019-08-02 16:13:07 +0700

    Cleanup wobble.js

--- js/wobble.js
+++ js/wobble.js
@@ -10,29 +10,33 @@ function initwobble() {
     if (document.getElementById) {
         let i;
         let wobli;
+
         wobble = document.getElementById("wobble");
         wobtxt = wobble.firstChild.nodeValue;
-        while (wobble.childNodes.length) wobble.removeChild(wobble.childNodes[0]);
+
+        while (wobble.childNodes.length) { wobble.removeChild(wobble.childNodes[0]); }
+
         for (i = 0; i < wobtxt.length; i++) {
-        wobli = document.createElement("span");
-        wobli.setAttribute("id", `wobb${i}`);
-        wobli.style.position = "relative";
-        wobli.appendChild(document.createTextNode(wobtxt.charAt(i)));
-        if (alink) {
-            wobli.style.cursor = "pointer";
-            wobli.onclick = () => {
-            top.location.href = alink;
-            };
-        }
-        wobble.appendChild(wobli);
+            wobli = document.createElement("span");
+            wobli.setAttribute("id", `wobb${i}`);
+            wobli.style.position = "relative";
+            wobli.appendChild(document.createTextNode(wobtxt.charAt(i)));
+            if (alink) {
+                wobli.style.cursor = "pointer";
+                wobli.onclick = () => {
+                    top.location.href = alink;
+                };
+            }
+            wobble.appendChild(wobli);
         }
         setInterval("wobbler()", speed);
     }
 }
 
 function wobbler() {
-    for (let i = 0; i < wobtxt.length; i++)
+    for (let i = 0; i < wobtxt.length; i++) {
         document.getElementById(`wobb${i}`).style.top =
         `${Math.round(height * Math.sin(i + wobcnt))}px`;
+    }
     wobcnt++;
 }
