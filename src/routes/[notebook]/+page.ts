4c5c9b8339b5b51a90672fb997647e87165bbdb8
Dania Rifki <atomicnumberphi@gmail.com>
2024-04-04 18:28:38 +0700

    Rename [slug] -> [notebook] and Update Deps

--- src/routes/[notebook]/+page.ts
+++ src/routes/[notebook]/+page.ts
@@ -2,13 +2,13 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
   try {
-    const post = await import(`../../notebook/${params.slug}.md`);
+    const post = await import(`../../notebook/${params.notebook}.md`);
 
     return {
       content: post.default,
       meta: post.metadata,
     };
   } catch (e) {
-    error(404, `Could not find ${params.slug}`);
+    error(404, `Could not find ${params.notebook}`);
   }
 }
